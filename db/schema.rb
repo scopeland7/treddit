657db4502d5d176bb3e13e282147cc450582788e
Sam Copeland <samcopeland7@gmail.com>
2016-02-04 13:12:11 -0500

    up to date with master branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160204162956) do
+ActiveRecord::Schema.define(version: 20160204164657) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
